07e5f8f2b4ad225127a0dc88fe71cca64641568f
ashivam22 <33142827+ashivam22@users.noreply.github.com>
2017-10-26 23:51:48 -0400

    FRONT END

---
+++ conf.php
@@ -0,0 +1,4 @@
+<script>
+	var MID_PATH="https://web.njit.edu/~sa839/MIDDLE/";
+ 
+</script>
---
+++ conf_test.php
@@ -0,0 +1,5 @@
+<?php
+	$GLOBALS['BACK_PATH']="https://web.njit.edu/~sa839/BACK/";
+	$GLOBALS['BACK_PATH']="http://localhost/online/part2/back/";
+	$GLOBALS['BACK_PATH']="http://localhost/Online_part2/back/";
+?>
